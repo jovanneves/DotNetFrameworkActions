44f19d9dc30fad81bc4d0ba721d6f47424f7af64
Jovan Neves <jovanneves@outlook.com>
2024-05-01 12:58:33 -0300

    Add testes

---
+++ src/DotNetFrameworkActions.UnitTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("DotNetFrameworkActions.UnitTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("DotNetFrameworkActions.UnitTest")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("efd89083-5945-459e-b13d-46e5b7eff085")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ src/DotNetFrameworkActions.UnitTest/ValidacaoProjetoUnitTest.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace DotNetFrameworkActions.UnitTest
+{
+    [TestClass]
+    public class ValidacaoProjetoUnitTest
+    {
+        [TestMethod]
+        public void EhValido()
+        {
+            Assert.IsTrue(true);
+        }
+
+        [TestMethod]
+        public void NaoEhValido()
+        {
+            Assert.IsFalse(false);
+        }
+    }
+}
