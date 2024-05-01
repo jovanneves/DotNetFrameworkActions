eb727fc4426128d72c3a264e3f190d16b4216355
Jovan Neves <jovanneves@outlook.com>
2024-05-01 12:11:06 -0300

    ss

--- DotNetFrameworkActions.WebApp/Properties/AssemblyInfo.cs
+++ DotNetFrameworkActions.WebApp/Properties/AssemblyInfo.cs
@@ -4,7 +4,7 @@ using System.Runtime.InteropServices;
 
 // As informações gerais sobre um assembly são controladas através do seguinte
 // conjunto de atributos a seguir. Altere esses valores de atributo para modificar as informações
-// associadas a um assembly.
+// associadas a um assembly..
 [assembly: AssemblyTitle("DotNetFrameworkActions.WebApp")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
